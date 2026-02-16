728b8186096187a5346fc6144846b80af2cc8a33
seolhee-git <pphh1034@gmail.com>
2026-02-16 13:12:12 +0900

    feat(references): 레퍼런스 등록 화면 초안 추가
    - /references/new 라우트와 전용 스크린을 추가함
    - 이미지 업로드/태그 선택/필수값 검증이 있는 등록 UI를 구현함
    - 등록/취소/닫기 동작을 /references로 라우팅하도록 연결함

---
+++ src/app/(protected)/references/new/page.tsx
@@ -0,0 +1,7 @@
+import { ReferenceCreateScreen } from "@/features/references/screens/reference-create-screen";
+
+export const dynamic = "force-dynamic";
+
+export default function ReferenceCreatePage() {
+  return <ReferenceCreateScreen />;
+}
