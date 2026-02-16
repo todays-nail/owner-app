e5693d3ab630bc2e744c5010815805993888c7ba
gumraze-git <galaxydh4110@gmail.com>
2026-02-16 21:26:54 +0900

    feat(ui): 디자인 아이템 수정 기능 및 상태 관리 로직 추가
    - 디자인 아이템 상태를 관리하는 useState 훅 추가
    - 디자인 아이템 수정(updateDesignItem) 함수 구현
    - 디자인 항목에 id 속성 추가 및 초기 데이터에 반영

--- src/features/dashboard/model/dashboard.ts
+++ src/features/dashboard/model/dashboard.ts
@@ -1,4 +1,5 @@
 export interface DashboardDesignItem {
+  id: string;
   name: string;
   price: string;
   image: string;
@@ -14,24 +15,28 @@ export interface DashboardScheduleItem {
 
 export const DASHBOARD_DESIGN_ITEMS: DashboardDesignItem[] = [
   {
+    id: "design-1",
     name: "골든 샌드",
     price: "₩75,000",
     image:
       "https://lh3.googleusercontent.com/aida-public/AB6AXuBMtvnCLpwiK62QJZEMACoktg6E0V_yzUy9b38JzKROLCoHL1iMcBklikV-MfjI81XokGcxGi4npM0FsL7ZFcixi9zlQ8VtAcyyJfa0k_izT_zEOHie-dpNyYwXJXNyAGJPHAvaQiut6nHg3f7yaLT2R0eHrKwoPfA-NUSJSpwlhrhTYH0ow0BlPsLZh-y0N6KA3ef4P2ZcwyrG6vsnEse27eccxhb-8k-nLg3VeCOSaTTpBadqRqVdKZYjrtSaPs1_UvgtZJDfodg"
   },
   {
+    id: "design-2",
     name: "아우라 핑크",
     price: "₩85,000",
     image:
       "https://lh3.googleusercontent.com/aida-public/AB6AXuADJ9s0xYk6wrfsXJE6CHcqWuxxJ-tdNYYVqveG2mplgSQCxgHrsd3nykgz_5rgYsbELmJcguwXG4UkuFyFb3VzIOYTJt0IfTkDRcIllg_E-M9i71BsktFzaCUvo_ktc6KoF2fvYGtwfK8bFMFjassAqGAlvCirE50EvrhjRap6paPugWJSBbYATXn_O6HYgK5kCN5sjYZit0WVqRlN26Pu2tnTu6VS-rP_aptI5VCavj_HdyDc2alLW8sYN6sT8y4Keabl2fGO0-o"
   },
   {
+    id: "design-3",
     name: "딥 씨 마블",
     price: "₩95,000",
     image:
       "https://lh3.googleusercontent.com/aida-public/AB6AXuARU91bmKufROkKoz257tweQ2rooH5bozrE_CYsK32VKP21s6QBkkswNU2EysCSsEnyvzRQSMNRqWowB_waFfN5UsQUBYvHrPc7YeaRW7yqc7IYQ-2wEcFmXwSAlkXmwi2yDdPPoclBhXCUiiyoMKITR3xSp5z6IfVdwncTX22M_p01Hbw58pVwWcNgDx2ptp7Qv_5wxL39a4AJd3vxZDjB5WyeGqhwCGT1IxTOD70QSkFNwuvZOWFBnlnn4AbdZZvwlfLdsnsErz4"
   },
   {
+    id: "design-4",
     name: "매트 스프링",
     price: "₩70,000",
     image:
